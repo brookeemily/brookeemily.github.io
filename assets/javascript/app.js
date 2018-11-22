63bec4eec4f115d9cdb30f79b96e8f55ac03ce41
brooke <brooke.mitchell@comcast.net>
2018-11-21 23:16:35 -0500

    add photo

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,52 +1,62 @@
-$(document).ready(function(){
-  showMenuIcon();
-  $("#menuButton").click(function(){
-    showCloseIcon();
-    $("#MainMenu").css("left","0px");
-    
-    function showMenu(){
-      $("#MainMenu").css("-webkit-clip-path","polygon(0 0,100% 0,100% 100%,0% 100%)");
-      //  $("#MenuIcon").animate({right:'-100'},300);
+   //change class function
+   function changeWidth() {
+    $('#portfolio').toggleClass('col s12');
+    $('#portfolio').toggleClass('col s10 offset-s2');
+    $('#bio').toggleClass('col s12');
+    $('#bio').toggleClass('col s10 offset-s2');
+  }
+
+$(document).ready(function() {
+  $(".iconBtnClose").hide();
+    changeWidth();
+
+    $('.tabs').tabs();
+
+  // When you click the menu icon....
+  $(".iconBtn").click(function() {
+    changeWidth();
+    // Main menu opens by setting css property left to 0 px
+    $("#mainMenu").css("left", "0px");
+    //  Function to show the menu
+    function showMenu() {
+      $("#mainMenu").css(
+        "-webkit-clip-path",
+        "polygon(0 0,100% 0,100% 100%,0% 100%)"
+      );
     }
-    setTimeout(showMenu,100);
-    setTimeout(changeBg, 350);
+
+    function hideIcon() {
+      $(".iconBtn").hide();
+      $(".iconBtnClose").show();
+    }
+
+ 
+    //  eases menu in
+    setTimeout(showMenu, 100);
+    setTimeout(hideIcon, 300);
   });
-   
-   $("#closeButton").click(function(){
-      showMenuIcon();
-           $("#MainMenu").css("-webkit-clip-path","polygon(0 0,0% 0,100% 100%,0% 100%)");
-           function hideMenu(){
-                   $("#MainMenu").css("left","-300px");
-              //  $("#MenuIcon").animate({right:'50'},300);
-           }
-       setTimeout(hideMenu,300);
-       
-       function originalLayout(){
-           $("#MainMenu").css("-webkit-clip-path","polygon(0 0,100% 0,0% 100%,0% 100%)");
-       }
-       setTimeout(originalLayout,600);
-       setTimeout(resetBg, 600);
-      });
+
+  //When you click the close button...
+  $(".iconBtnClose").click(function() {
+    setTimeout(changeWidth, 425);
+
+    //  Main menu gets hidden - set back to 0%
+    $("#mainMenu").css(
+      "-webkit-clip-path",
+      "polygon(0 0,0% 0,100% 100%,0% 100%)"
+    );
+    // Function to hide menu
+    function hideMenu() {
+      // Sets main menu css to -300px left, hiding the main menu
+      $("#mainMenu").css("left", "-300px");
+    }
+
+    function hideIcons() {
+      $(".iconBtn").show();
+      $(".iconBtnClose").hide();
+    }
+    // eases menu out
+    setTimeout(hideMenu, 175);
+    setTimeout(hideIcons, 300);
+  });
 });
-
-function showMenuIcon() {
-  $("#menuButton").show();
-  $("#closeButton").hide();
-}
-
-function showCloseIcon() {
-  $("#closeButton").show();
-  $("#menuButton").hide();
-}
-
-function changeBg() {
-  $("#content").css('background-color', 'rgb(11, 3, 21)');
-  $("body").css('background-color', 'rgb(11, 3, 21)');
-}
-
-
-function resetBg() {
-  $("#content").css('background-color', 'rgb(27, 4, 34)');
-  $("body").css('background-color', 'rgb(27, 4, 34)');
-
-}
